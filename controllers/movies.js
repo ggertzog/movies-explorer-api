c2e878d493d2040b3dc4c9c5743daa04fd441f8a
ggertzog <musienko.roma@mail.ru>
2023-12-07 12:17:19 +0300

    bug-fix2

--- controllers/movies.js
+++ controllers/movies.js
@@ -6,10 +6,7 @@ const { RESPONSE_CODE } = require('../utils/constants');
 module.exports.getSavedMovies = async ( req, res, next ) => {
   try{
     const movies = await movieModel.find({ owner: req.user._id });
-    if(!movies.length) {
-      return res.satus(RESPONSE_CODE.NOT_FOUND).send({ message: 'Список ваших фильмов пуст'});
-    }
-      return res.send(movies);
+    return res.status(RESPONSE_CODE.OK).send(movies);
   } catch (err) {
     return next(err);
   }
